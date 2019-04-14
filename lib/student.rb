ea14e4e1406de8f9451bb361fd46d57d43d27eaf
Eric Storey <estorey@gmail.com>
2019-04-14 19:49:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,10 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new  # self.new is the same as running Song.new
-    new_song.id = row[0]
-    new_song.name =  row[1]
-    new_song.length = row[2]
-    new_song
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
